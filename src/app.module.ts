bb3bec72910b640c9ff02ae4e47b3d602bb5f8d8
ClydeRM <clyde.official@icloud.com>
2022-05-18 20:52:36 +0800

    feat(Module): Setup Graphql module
    // Looking all Graphql schema
    typePaths: ['./**/*.graphql']
    
    // Turn off GQL playground
    playground: false.
    
    // Use Apollo sandbox replace GQL playground
    plugins: [ApolloServerPluginLandingPageLocalDefault()]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'], // Looking all Graphql schema
+      playground: false, // Turn off GQL playground
+      plugins: [ApolloServerPluginLandingPageLocalDefault()], // Use Apollo sandbox replace GQL playground
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
