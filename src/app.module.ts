cc8502ab4feb3f58c79f094b6b266736693bc751
ClydeRM <clyde.official@icloud.com>
2022-05-18 21:07:14 +0800

    feat: Init Donation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AppService } from './app.service';
       playground: false, // Turn off GQL playground
       plugins: [ApolloServerPluginLandingPageLocalDefault()], // Use Apollo sandbox replace GQL playground
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
