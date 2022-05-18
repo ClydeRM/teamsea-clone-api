cff3669275c33f292083cf0f159bd9eb16a04dab
ClydeRM <clyde.official@icloud.com>
2022-05-18 21:38:49 +0800

    feat(Module): Add gql-ios-date resolvers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphqlDateTime } from 'graphql-iso-date';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DonationsModule } from './donations/donations.module';
@@ -13,6 +14,7 @@ import { DonationsModule } from './donations/donations.module';
       typePaths: ['./**/*.graphql'], // Looking all Graphql schema
       playground: false, // Turn off GQL playground
       plugins: [ApolloServerPluginLandingPageLocalDefault()], // Use Apollo sandbox replace GQL playground
+      resolvers: { DateTime: GraphqlDateTime }, // Use for timestamp scalar
     }),
     DonationsModule,
   ],
