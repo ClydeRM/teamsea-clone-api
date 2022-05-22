7b4b4dc670afe6049522e5c0c2ef9051346fa080
ClydeRM <clyde.official@icloud.com>
2022-05-22 21:05:38 +0800

    build(Typings): Ref to *.graphql schema

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -7,25 +8,40 @@
 /* tslint:disable */
 /* eslint-disable */
 export class CreateDonationInput {
-  exampleField?: Nullable<number>;
+    count?: Nullable<number>;
+    displayName: string;
+    email: string;
+    mobile?: Nullable<string>;
+    team?: Nullable<string>;
+    message?: Nullable<string>;
+    createdAt?: Nullable<DateTime>;
+}
+
+export class OrderByParams {
+    field?: Nullable<string>;
+    direction?: Nullable<string>;
 }
 
 export class Donation {
-  exampleField?: Nullable<number>;
+    id: number;
+    count?: Nullable<number>;
+    displayName: string;
+    email: string;
+    mobile?: Nullable<string>;
+    team?: Nullable<string>;
+    message?: Nullable<string>;
+    createdAt?: Nullable<DateTime>;
 }
 
 export abstract class IQuery {
-  abstract donations(): Nullable<Donation>[] | Promise<Nullable<Donation>[]>;
+    abstract donations(orderBy?: Nullable<OrderByParams>): Nullable<Donation>[] | Promise<Nullable<Donation>[]>;
 
-  abstract donation(
-    id: number,
-  ): Nullable<Donation> | Promise<Nullable<Donation>>;
+    abstract donation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>;
 }
 
 export abstract class IMutation {
-  abstract createDonation(
-    createDonationInput: CreateDonationInput,
-  ): Donation | Promise<Donation>;
+    abstract createDonation(createDonationInput: CreateDonationInput): Donation | Promise<Donation>;
 }
 
+export type DateTime = any;
 type Nullable<T> = T | null;
