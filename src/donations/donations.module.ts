0ea5ee4c2252d58eb9c86d3a3cdeeb1101e24a20
ClydeRM <clyde.official@icloud.com>
2022-05-22 18:03:43 +0800

    refactor(Module): GlobalPrismaService

--- src/donations/donations.module.ts
+++ src/donations/donations.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DonationsService } from './donations.service';
 import { DonationsResolver } from './donations.resolver';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [DonationsResolver, DonationsService, PrismaService],
+  providers: [DonationsResolver, DonationsService],
 })
 export class DonationsModule {}
