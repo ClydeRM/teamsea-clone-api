a71dfb172e742069b012850cdcf3930414456705
ClydeRM <clyde.official@icloud.com>
2022-05-23 17:13:07 +0800

    feat(Resolver): Implement CR methods
    Implement subscriptions getTotal update in real time

--- src/donations/donations.resolver.ts
+++ src/donations/donations.resolver.ts
@@ -1,25 +1,52 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Subscription } from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
+
 import { DonationsService } from './donations.service';
-import { CreateDonationInput } from './dto/create-donation.input';
+
+import { DonationCreateInput } from '../@generated/prisma-nestjs-graphql/donation/donation-create.input';
+import { OrderByParams } from '../graphql';
+
+// Subscription showing real time donations count update
+const pubSub = new PubSub();
 
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
 
   @Mutation('createDonation')
-  create(
-    @Args('createDonationInput') createDonationInput: CreateDonationInput,
+  async create(
+    @Args('createDonationInput')
+    createDonationInput: DonationCreateInput, //Prisma.DonationCreateInput
   ) {
-    return this.donationsService.create(createDonationInput);
+    // Save the donation
+    const created = await this.donationsService.create(createDonationInput);
+
+    // Calculate new Update getTotal() data
+    const total = await this.donationsService.getTotal();
+
+    // Call Subscription publish('API Schema', { Obj });
+    pubSub.publish('totalUpdated', { totalUpdated: { total } });
+
+    return created;
+  }
+
+  @Subscription('totalUpdated')
+  totalUpdated() {
+    return pubSub.asyncIterator('totalUpdated');
   }
 
   @Query('donations')
-  findAll() {
-    return this.donationsService.findAll();
+  findAll(@Args('orderBy') orderBy?: OrderByParams) {
+    return this.donationsService.findAll(orderBy);
   }
 
   @Query('donation')
   findOne(@Args('id') id: number) {
-    return this.donationsService.findOne(id);
+    return this.donationsService.findOne({ id });
+  }
+
+  @Query('totalDonations')
+  totalDonations() {
+    return this.donationsService.getTotal();
   }
 }
