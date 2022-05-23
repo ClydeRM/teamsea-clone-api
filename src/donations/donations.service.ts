56fe3156ae21406e4cb4c52463e8cf050388d5ad
ClydeRM <clyde.official@icloud.com>
2022-05-23 17:13:58 +0800

    feat(Service): Implement CR methods
    Use Schema's types safty to insure type safety

--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDonationInput } from './dto/create-donation.input';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+import { OrderByParams } from '../graphql';
+import { DonationCreateInput } from '../@generated/prisma-nestjs-graphql/donation/donation-create.input';
 
 @Injectable()
 export class DonationsService {
-  create(createDonationInput: CreateDonationInput) {
-    return 'This action adds a new donation';
+  constructor(private readonly prisma: PrismaService) {}
+  create(createDonationInput: DonationCreateInput) {
+    return this.prisma.donation.create({
+      data: createDonationInput,
+    });
   }
 
-  findAll() {
-    return `This action returns all donations`;
+  findAll(orderBy?: OrderByParams) {
+    // Select orderBy present or count desc
+    const { field = 'createdAt', direction = 'desc' } = orderBy || {};
+    return this.prisma.donation.findMany({
+      orderBy: { [field]: direction }, // [obj]: obj dynamic use obj
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donation`;
+  findOne(donationWhereUniqueInput: Prisma.DonationWhereUniqueInput) {
+    return this.prisma.donation.findUnique({
+      where: donationWhereUniqueInput,
+    });
+  }
+
+  async getTotal() {
+    const response = await this.prisma.donation.aggregate({
+      _sum: {
+        count: true,
+      },
+    });
+    return response._sum.count;
   }
 }
