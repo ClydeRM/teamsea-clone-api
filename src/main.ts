32a16726f209033ef557838c875a8688a39aa8a5
ClydeRM <clyde.official@icloud.com>
2022-05-23 17:08:40 +0800

    chore(Main): Turn off whitelist

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // 限制request，只傳入DTO規定的內容
-      transform: true, // 自動轉換 request Body, 將Body的型別成DTO的類別物件
-      forbidNonWhitelisted: true, // 抵擋request，如果request body有DTO規定外的欄位，request被攔截
+      // whitelist: true, // 限制request，只傳入DTO規定的內容
+      // transform: true, // 自動轉換 request Body, 將Body的型別成DTO的類別物件
+      // forbidNonWhitelisted: true, // 抵擋request，如果request body有DTO規定外的欄位，request被攔截
     }),
   );
 
